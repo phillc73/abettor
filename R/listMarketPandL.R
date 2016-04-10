d27934cf914658285357ea601492578ac7d0a912
dashee87 <breakof93@hotmail.com>
2016-04-10 23:33:09 +0100

    Update listMarketPandL.R
    Making marketIds argument required, adding basic error handling and fixing typos

--- R/listMarketPandL.R
+++ R/listMarketPandL.R
@@ -20,10 +20,8 @@
 #'   list).
 #'
 #' @param marketIds Vector<String>. A set of market ID strings from which the
-#'   corresponding market profit and losses will be returned. According to the
-#'   online documentation, this parameter is not required. However, setting it
-#'   to null will return an empty data frame. So I suggest including market IDs
-#'   in this parameter.
+#'   corresponding market profit and losses will be returned. Required.
+#'   No default.
 #'
 #' @param includeSettledBetsValue Boolean. Option to include settled bets
 #'   (partially settled markets only). This parameter defaults to NULL, which
@@ -45,24 +43,25 @@
 #'   error occurs you may set sslVerify to FALSE. This does open a small
 #'   security risk of a man-in-the-middle intercepting your login credentials.
 #'
-#' @param errorWarning Boolean. This argument defaults to FALSE and is optional.
-#'   If set to TRUE, then, in the case of errors, the error details will be
-#'   posted as a warning.
+#'@param suppress Boolean. By default, this parameter is set to FALSE, meaning 
+#'   that a warning is posted when the listMarketPandL call throws an error. 
+#'   Changing this parameter to TRUE will suppress this warning.
 #'
 #' @return Response from Betfair is stored in listPandL variable, which is then
 #'   parsed from JSON as a data frame of at least two varialbes (more if the
 #'   optional parameters are included). The first column records the market IDs,
 #'   while the corresponding market P&Ls are stored within a list.
 #'
-#' @section Note on \code{listMarketBookOps} variable: The
-#'   \code{listMarketBookOps} variable is used to firstly build an R data frame
+#' @section Note on \code{listPandLOps} variable: The
+#'   \code{listPandLOps} variable is used to firstly build an R data frame
 #'   containing all the data to be passed to Betfair, in order for the function
 #'   to execute successfully. The data frame is then converted to JSON and
-#'   included in the HTTP POST request.
+#'   included in the HTTP POST request. If the listMarketPandL call throws an 
+#'   error, a data frame containing error information is returned.
 #'
 #' @examples
 #' \dontrun{
-#' Return the P&L (net of comission) for the requested markets. This actual
+#' Return the P&L (net of comission) for the requested markets. The actual
 #' market IDs are unlikely to work and are just for demonstration purposes.
 #'
 #' listMarketPandL(marketIds = c("1.122323121","1.123859413"),
@@ -72,33 +71,32 @@
 
 
 listMarketPandL <-
-  function(marketIds = NULL,includeSettledBetsValue = NULL,includeBspBetsValue =
-             NULL,netOfCommissionValue = NULL,sslVerify = TRUE,errorWarning = FALSE) {
+  function(marketIds, includeSettledBetsValue = NULL,includeBspBetsValue = NULL,
+           netOfCommissionValue = NULL, suppress = FALSE, sslVerify = TRUE) {
     options(stringsAsFactors = FALSE)
-
+    
     listPandLOps <-
       data.frame(jsonrpc = "2.0", method = "SportsAPING/v1.0/listMarketProfitAndLoss", id = "1")
-
-    listPandLOps$params <- data.frame(includeSettledBetsValue = "")
-    if (!is.null(marketIds))
-      listPandLOps$params$marketIds <- list(c(marketIds))
-
+    
+    listPandLOps$params <- data.frame(marketIds = c(marketIds))
+    listPandLOps$params$marketIds <- list(listPandLOps$params$marketIds)
+    
     listPandLOps$params$includeSettledBets <- includeSettledBetsValue
     listPandLOps$params$includeBspBets <- includeBspBetsValue
     listPandLOps$params$netOfCommission <- netOfCommissionValue
-
+    
     listPandLOps <- listPandLOps[c("jsonrpc", "method", "params", "id")]
-
+    
     listPandLOps <- jsonlite::toJSON(listPandLOps, pretty = TRUE)
-
+    print(listPandLOps)
     # Read Environment variables for authorisation details
     product <- Sys.getenv('product')
     token <- Sys.getenv('token')
-
+    
     headers <- list(
       'Accept' = 'application/json', 'X-Application' = product, 'X-Authentication' = token, 'Content-Type' = 'application/json'
     )
-
+    
     listPandL <-
       jsonlite::fromJSON(
         RCurl::postForm(
@@ -107,14 +105,10 @@ listMarketPandL <-
           )
         )
       )
-
-    if (errorWarning) {
-      if (!is.null(listPandL$error))
-        warning(
-          paste0(
-            "FAIL: ",listPandL$error$data$APINGException$errorCode," (",listPandL$error$data$APINGException$errorDetails,")"
-          )
-        )
-    }
-    as.data.frame(listPandL$result)
+    if(is.null(listPandL$error))
+      as.data.frame(listPandL$result)
+    else({
+      if(!suppress)
+        warning("Error- See output for details")
+      as.data.frame(listPandL$error)})
   }
