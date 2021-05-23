bef179351ad9a2111d6dd9f5cbd5919cefb1b770
Soccerama <theportfoliopunter@mail.com>
2021-05-23 19:30:40 +1000

    Change to address issue where output was not correctly returning a dataframe

--- R/listClearedOrders.R
+++ R/listClearedOrders.R
@@ -153,9 +153,11 @@ listClearedOrders <-
       httr::POST(url = "https://api.betfair.com/exchange/betting/json-rpc/v1",
                  config = httr::config(ssl_verifypeer = sslVerify),
                  body = listOrderOps,
-                 httr::add_headers(Accept = "application/json", `X-Application` = product, `X-Authentication` = token)
-      )
-    )
+                 httr::add_headers(Accept = "application/json",
+                                   "X-Application" = product,
+                                   "X-Authentication" = token)), as = "text", encoding = "UTF-8")
+
+    listOrder <- jsonlite::fromJSON(listOrder)
 
      if (!is.null(listOrder$error)){
       if(!suppress)
